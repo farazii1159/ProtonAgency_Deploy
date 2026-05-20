ea419164adf9435e952fc34b1c61a31ef2e3b3ed
Faraz Shabbir <farazii1159@gmail.com>
2026-05-20 15:26:06 +0500

    update favicon error

--- app/Work/layout.js
+++ app/Work/layout.js
@@ -14,6 +14,9 @@ const mono = JetBrains_Mono({
 export const metadata = {
   title: "Proton || Build Your Dream Brand",
   description: "Proton || Build Your Dream Brand",
+  icons: {
+    icon: "/favicon.ico", 
+  },
 };
 
 export default function RootLayout({ children }) {
